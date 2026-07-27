1b6fff509c157a701064319d4dd5b7df06887fba
Aamit Baran Pattanayak <aamitbaranpattanayak@gmail.com>
2026-07-27 23:24:14 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { initialCategories, initialEvents, initialBookings } from "./mockData.js";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://bookora-backend-7juo.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
